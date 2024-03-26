6dab2b28aabe57e7f3b515cd06ed79619bc84ab0
Thiago Rodrigues Padovani <thiagorodriguespadovani@MacBook-Pro-de-Thiago.local>
2024-03-26 09:42:37 -0300

    add missing translation in education info

--- src/ui/pages/About/EducationInfo/EducationInfo.tsx
+++ src/ui/pages/About/EducationInfo/EducationInfo.tsx
@@ -1,11 +1,13 @@
 import { Box } from '@mui/material'
+import { useTranslation } from 'react-i18next'
 import { educationInfo } from './EducationInfo.utils'
 import Info from '../Info'
 
 export default function EducationInfo() {
+  const { t } = useTranslation()
   return (
     <Box display="flex" flexDirection="column" gap={2}>
-      {educationInfo.map(({ id, title, company, location, startDate, endDate }) => (
+      {educationInfo(t).map(({ id, title, company, location, startDate, endDate }) => (
         <Info
           key={id}
           title={title}
